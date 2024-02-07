5eea3254eb83378d0c3c938087481910f29a75f4
Deyvis ML <deyvisml2002@gmail.com>
2024-02-07 12:59:03 -0500

    feat: button inside ProductItem component fixed

--- frontend/src/components/Products/ProductItem.jsx
+++ frontend/src/components/Products/ProductItem.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const ProductItem = ({ id, name, price }) => {
   console.log(id);
   return (
-    <article className="overflow-hidden bg-white border rounded-md shadow shadow-gray-300 group hover:text-purple-700">
-      <Link to={`${id}`} className="block p-2">
+    <article className="p-2 overflow-hidden bg-white border rounded-md shadow shadow-gray-300 group hover:text-purple-700">
+      <Link to={`${id}`} className="block">
         <div className="w-full mb-1 overflow-hidden h-52">
           <img
             src="https://fastly.picsum.photos/id/990/768/1024.jpg?hmac=FmdIxAjPutGawu1QkxbXL63lY65sO4kJHAcWBV7inYs"
@@ -17,10 +17,15 @@ const ProductItem = ({ id, name, price }) => {
           <p className="mb-1 leading-5 h-9">{name}.</p>
           <p className="text-xl font-semibold">{price}</p>
         </div>
-        <button className="w-full p-1 py-1.5 font-semibold hover:shadow-lg text-xs text-center text-white uppercase bg-purple-800 rounded-xl">
-          Agregar
-        </button>
       </Link>
+      <button
+        onClick={() => {
+          alert("xd");
+        }}
+        className="w-full p-1 py-1.5 font-semibold hover:shadow-lg text-xs text-center text-white uppercase bg-purple-800 rounded-xl"
+      >
+        Agregar
+      </button>
     </article>
   );
 };
