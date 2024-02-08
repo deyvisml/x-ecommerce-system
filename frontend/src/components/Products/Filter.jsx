c9e38653fc2a3ae60c4d7f1a312bc1208c306e4f
Deyvis ML <deyvisml2002@gmail.com>
2024-02-08 10:38:08 -0500

    feat: loader and skeleton integrated in products component

--- frontend/src/components/Products/Filter.jsx
+++ frontend/src/components/Products/Filter.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const Filter = ({ items = [], filter_item_id, setFilterItemId }) => {
+const Filter = ({ items, filter_item_id, setFilterItemId }) => {
   const handle_filter_item_click = (e) => {
     const filter_item_id = e.target.dataset.filterItemId;
     setFilterItemId(filter_item_id);
@@ -12,21 +14,22 @@ const Filter = ({ items = [], filter_item_id, setFilterItemId }) => {
         Filtrar por tipo
       </span>
 
-      <ul className="flex flex-wrap gap-1 text-xs">
-        {items.map(({ name, id }) => {
-          return (
-            <li
-              key={id}
-              data-filter-item-id={id}
-              onClick={handle_filter_item_click}
-              className={`cursor-pointer hover:bg-purple-200 transition-all ease-in-out duration-100 px-3.5 py-1.5 text-center border border-gray-500 rounded-full ${
-                filter_item_id == id && "bg-purple-200"
-              }`}
-            >
-              {name}
-            </li>
-          );
-        })}
+      <ul className="flex flex-wrap gap-1 text-xs min-w-20 min-h-4">
+        {(items &&
+          items.map(({ name, id }) => {
+            return (
+              <li
+                key={id}
+                data-filter-item-id={id}
+                onClick={handle_filter_item_click}
+                className={`cursor-pointer hover:bg-purple-200 transition-all ease-in-out duration-100 px-3.5 py-1.5 text-center border border-gray-500 rounded-full ${
+                  filter_item_id == id && "bg-purple-200"
+                }`}
+              >
+                {name}
+              </li>
+            );
+          })) || <Skeleton containerClassName="flex-1" height={25} />}
       </ul>
     </div>
   );
--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { GridLoader } from "react-spinners";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 import { useParams } from "react-router-dom";
 import axios_client from "../../helpers/axios";
 
@@ -9,9 +13,16 @@ import ProductItem from "./ProductItem";
 const Products = () => {
   const { category_id } = useParams();
 
-  const [product_types, setProductTypes] = useState([]);
-  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState();
+  const [product_types, setProductTypes] = useState();
+  const [products, setProducts] = useState();
   const [filter_item_id, setFilterItemId] = useState("");
+
+  const fetch_category = async (category_id) => {
+    const { data } = await axios_client(`api/categories/${category_id}`);
+
+    setCategory(data.data);
+  };
 
   const fetch_product_types_by_category = async (category_id) => {
     const { data } = await axios_client(
@@ -30,38 +41,55 @@ const Products = () => {
   };
 
   useEffect(() => {
+    fetch_category(category_id);
     fetch_product_types_by_category(category_id);
   }, []);
 
   useEffect(() => {
-    fetch_products_by_category(category_id, filter_item_id);
+    setProducts();
+    setTimeout(() => {
+      fetch_products_by_category(category_id, filter_item_id);
+    }, 500);
   }, [filter_item_id]);
 
   return (
     <div className="text-gray-800">
       <div className="mb-2">
-        <h3 className="text-2xl font-bold">Ramo de flores</h3>
+        <h3 className="text-2xl font-bold">
+          {(category && category.name) || <Skeleton />}
+        </h3>
       </div>
       <div className="flex flex-col items-start mb-3 sm:items-end">
-        <Filter
-          items={product_types}
-          filter_item_id={filter_item_id}
-          setFilterItemId={setFilterItemId}
-        />
+        {
+          <Filter
+            items={product_types}
+            filter_item_id={filter_item_id}
+            setFilterItemId={setFilterItemId}
+          />
+        }
       </div>
 
       <section className="grid grid-cols-1 gap-4 md:gap-x-3 md:gap-y-4 md:grid-cols-3 lg:grid-cols-5 ">
-        {products.map(({ id, name, image_url, price }) => {
-          return (
-            <ProductItem
-              key={id}
-              id={id}
-              name={name}
-              image_url={image_url}
-              price={price}
+        {products ? (
+          products.map(({ id, name, image_url, price }) => {
+            return (
+              <ProductItem
+                key={id}
+                id={id}
+                name={name}
+                image_url={image_url}
+                price={price}
+              />
+            );
+          })
+        ) : (
+          <div className="col-span-5 text-center h-60 py-14">
+            <GridLoader
+              className="w-full text-center text-gray-300"
+              color="#a855f7"
             />
-          );
-        })}
+          </div>
+        )}
       </section>
     </div>
   );
