4d56a85bd1d63a8e07d8af5ffcb83bf256f16554
Deyvis <deyvisml2002@gmail.com>
2024-02-28 19:20:22 -0500

    feat: using a usd exchange rate value, now the price is in the right usd value

--- frontend/src/components/Order/PaypalModal.jsx
+++ frontend/src/components/Order/PaypalModal.jsx
@@ -1,4 +1,5 @@
 import { Fragment, useState, useEffect } from "react";
+import axios_client from "../../helpers/axios";
 import { Dialog, Transition } from "@headlessui/react";
 
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
@@ -6,6 +7,7 @@ import { FaXmark } from "react-icons/fa6";
 import useECommerce from "../../hooks/useECommerce";
 
 const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
+  const [usd_exchange_rate, setUsdExchangeRate] = useState();
   const { cart } = useECommerce();
 
   const closeModal = () => {
@@ -13,6 +15,19 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
   };
 
   const [{ isPending }] = usePayPalScriptReducer();
+
+  const fetch_exchange_rate = async (usd_exchange_rate_id) => {
+    const { data } = await axios_client(
+      `api/exchange-rates/${usd_exchange_rate_id}`
+    );
+
+    setUsdExchangeRate(data.data.price);
+  };
+
+  useEffect(() => {
+    const usd_exchange_rate_id = 1;
+    fetch_exchange_rate(usd_exchange_rate_id);
+  }, []);
 
   const createOrder = (data, actions) => {
     let item_total_price = 0;
@@ -24,10 +39,10 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
         quantity,
       } = item;
 
-      name = name.length > 127 ? name.slice(0, 125) + "..." : name;
+      name = name.length > 127 ? name.slice(0, 125) + ".." : name;
       description =
         description.length > 127
-          ? description.slice(0, 20) + "..."
+          ? description.slice(0, 20) + ".."
           : description;
 
       items.push({
@@ -36,11 +51,11 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
         description: description,
         unit_amount: {
           currency_code: "USD",
-          value: price,
+          value: (price / usd_exchange_rate).toFixed(2),
         },
       });
 
-      item_total_price += price * quantity;
+      item_total_price += (price / usd_exchange_rate).toFixed(2) * quantity;
     }
 
     item_total_price = item_total_price.toFixed(2);
@@ -51,7 +66,9 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
           description: `Florecer contigo`,
           amount: {
             currency_code: "USD",
-            value: Number(item_total_price) + Number(delivery_cost),
+            value:
+              Number(item_total_price) +
+              Number((delivery_cost / usd_exchange_rate).toFixed(2)),
             breakdown: {
               item_total: {
                 currency_code: "USD",
@@ -59,7 +76,7 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
               },
               shipping: {
                 currency_code: "USD",
-                value: delivery_cost,
+                value: (delivery_cost / usd_exchange_rate).toFixed(2),
               },
             },
           },
@@ -73,7 +90,6 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
   const onApprove = (data, actions) => {
     return actions.order.capture().then(function (details) {
       const { payer } = details;
-      setSuccess(true);
     });
   };
 
@@ -134,72 +150,5 @@ const PaypalModal = ({ is_open_modal, setIsOpenModal, delivery_cost }) => {
     </Transition>
   );
 };
-
-/*
-const init_paypal_button = (items) => {
-  const container = document.querySelector("#paypal-button-container");
-  container.innerHTML = "";
-  let cantidad = 1;
-  paypal
-    .Buttons({
-      style: {
-        shape: "rect",
-        color: "",
-        layout: "vertical",
-        label: "pay",
-      },
-      createOrder: function (data, actions) {
-        let total_price = 0;
-        const items = [];
-        for (const item of items) {
-          let { title, product_description, sku, discounted_price, quantity } =
-            item;
-          discounted_price /= 100;
-          items.push({
-            name: title,
-            description: "sss",
-            sku: sku,
-            unit_amount: {
-              currency_code: "USD",
-              value: discounted_price,
-            },
-            quantity: quantity,
-          });
-          total_price += discounted_price * quantity;
-        }
-        total_price = total_price.toFixed(2);
-        return actions.order.create({
-          purchase_units: [
-            {
-              description: `Cosmos Creativo Order`,
-              amount: {
-                currency_code: "USD",
-                value: total_price,
-                breakdown: {
-                  item_total: {
-                    currency_code: "USD",
-                    value: total_price,
-                  },
-                },
-              },
-              items: items,
-            },
-          ],
-        });
-      },
-      onApprove: function (data, actions) {
-        return actions.order.capture().then(function (details) {
-          window.location.href = "https://cosmoscreativo.shop/";
-        });
-      },
-      onError: function (err) {
-        console.log(err);
-        alert("Ocurrio un error al procesar el pago :(, intentelo nuevamente.");
-      },
-    })
-    .render("#paypal-button-container");
-};
-
-init_paypal_button(items);*/
 
 export default PaypalModal;
