4eab6f10f47b479517a803d39995f7611c9e3402
Deyvis <deyvisml2002@gmail.com>
2024-05-03 10:51:17 -0500

    feat: ProductItem fixed, delete add to cart button, now it's view product.

--- frontend/client/src/components/Products/ProductItem.jsx
+++ frontend/client/src/components/Products/ProductItem.jsx
@@ -1,36 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import addItemToCart from "../../utils/addItemToCart";
-import { v4 as uuidv4 } from "uuid";
 import currency from "currency.js";
 
-import useECommerce from "../../hooks/useECommerce";
-
 const ProductItem = ({ product }) => {
-  const { cart, setCart } = useECommerce();
-
-  const handle_add_product_to_cart_btn = () => {
-    const item = {
-      id: uuidv4(),
-      product,
-      quantity: 1,
-    };
-
-    const [error_occurred, message] = addItemToCart(item, cart, setCart);
-
-    if (!error_occurred) {
-      toast.success(message);
-    } else {
-      toast.error(message);
-    }
-  };
-
   return (
-    <article className="flex flex-col justify-between bg-white shadow shadow-gray-300 p-2.5 rounded-md hover:text-rose-600 overflow-hidden group">
+    <article className="flex flex-col justify-between bg-white shadow shadow-gray-300 hover:shadow-lg rounded-md hover:text-rose-600 overflow-hidden group">
       <Link
         to={`/categorias/${product.category_id}/productos/${product.id}`}
-        className="block"
+        className="block p-2.5 group"
       >
         <div className="mb-1 w-full h-52 overflow-hidden">
           <img
@@ -41,7 +18,7 @@ const ProductItem = ({ product }) => {
             className="group-hover:scale-125 w-full h-full transform transition-transform object-contain"
           />
         </div>
-        <div className="mb-2 text-center">
+        <div className="text-center">
           {product.in_offer == true && (
             <div className="mb-2 w-full text-left uppercase">
               <p className="inline-block bg-gray-600 px-1 py-0.5 rounded text-white text-xs">
@@ -68,19 +45,15 @@ const ProductItem = ({ product }) => {
             )}
           </div>
         </div>
-      </Link>
 
-      <div className="mx-4">
-        <button
-          disabled={product.in_stock ? false : true}
-          onClick={handle_add_product_to_cart_btn}
-          className={`inline-block ${
-            product.in_stock ? "bg-rose-500" : "bg-red-400"
-          } hover:shadow-lg py-1.5 p-1 rounded-xl w-full font-semibold text-center text-white text-xs uppercase`}
-        >
-          {product.in_stock ? "Agregar" : "Agotado"}
-        </button>
-      </div>
+        <div className="group-hover:visible mx-2 invisible">
+          <span
+            className={`inline-block bg-rose-500 hover:shadow-lg py-1.5 p-1 rounded-xl w-full font-semibold text-center text-white text-xs `}
+          >
+            Ver producto
+          </span>
+        </div>
+      </Link>
     </article>
   );
 };
