0dc5d1ff714be3e2bfbeb94d42893b1d635a4b73
Deyvis <deyvisml2002@gmail.com>
2024-05-07 11:26:50 -0500

    feat: button ofertas updated.

--- frontend/client/src/components/Header/Header.jsx
+++ frontend/client/src/components/Header/Header.jsx
@@ -119,15 +119,8 @@ const Header = () => {
 
   return (
     <>
-      <header className="md:block top-0 z-30 flex flex-col gap-y-2 bg-whtie shadow-md border-b w-full">
-        <div className="bg-rose-700 text-white text-xs">
-          <button
-            onClick={handle_go_offer_product_btn}
-            className="block p-1.5 w-full text-center uppercase"
-          >
-            Ofertas del dia
-          </button>
-        </div>
+      <header className="md:block top-0 z-30 sticky flex flex-col gap-y-2 bg-white bg-whtie shadow-gray-400 shadow-md border-b w-full">
+        <div className="bg-rose-700 h-5 text-white text-xs"></div>
 
         <div className="mx-auto px-2 xl:px-0 w-full max-w-7xl">
           <div className="flex flex-wrap justify-between items-center">
@@ -153,30 +146,6 @@ const Header = () => {
                 </button>
               </form>
             </div>
-
-            <div className="flex gap-x-4 order-2 md:order-3">
-              <DropdownItem boton1={<button type="submit">btn</button>} />
-              <Link to="carrito-compras" className="inline-block font-semibold">
-                <div className="relative">
-                  <ShoppingCartIcon className="w-10 text-rose-500" />
-                  {cart && cart.items.length > 0 && (
-                    <div className="-top-2 -right-1 absolute flex justify-center items-center border-white bg-rose-500 mt-1 border rounded-full w-5 h-5 text-white text-xs transform">
-                      <span
-                        className="inline-block font-bold"
-                        style={{
-                          marginBottom: -1,
-                        }}
-                      >
-                        {cart.items.reduce(
-                          (sum, item) => sum + item.quantity,
-                          0
-                        )}
-                      </span>
-                    </div>
-                  )}
-                </div>
-              </Link>
-            </div>
           </div>
         </div>
 
@@ -191,42 +160,40 @@ const Header = () => {
                   setIsOpenMenu((current_value) => !current_value);
                 }}
               >
-                <Bars3Icon className="w-8" />
+                <Bars3Icon className="bg-none w-8 text-gray-500" />
               </button>
-              {
-                <ul
-                  className={`${
-                    is_open_menu ? "block" : "hidden"
-                  } md:relative right-0 bottom-0 absolute md:flex flex-wrap items-center bg-white divide-x divide-y md:divide-y-0 text-rose-500 text-xs uppercase transform translate-y-full md:translate-y-0`}
-                >
-                  {menu_items.map((menu_item) => {
-                    return (
-                      <li
-                        key={menu_item.id}
-                        className="border-neutral-300 w-full md:w-auto hover:text-rose-300 transition-all duration-300 ease-in-out"
-                      >
-                        {menu_item.has_subitems ? (
-                          <DropdownItem subitems={menu_item.subitems}>
-                            <Link
-                              to={menu_item.url}
-                              className="inline-block px-4 py-2 font-bold"
-                            >
-                              {menu_item.name}
-                            </Link>
-                          </DropdownItem>
-                        ) : (
+              <ul
+                className={`${
+                  is_open_menu ? "block" : "hidden"
+                } md:relative right-0 bottom-0 absolute md:flex flex-wrap items-center bg-white divide-x divide-y md:divide-y-0 text-rose-500 text-xs uppercase transform translate-y-full md:translate-y-0`}
+              >
+                {menu_items.map((menu_item) => {
+                  return (
+                    <li
+                      key={menu_item.id}
+                      className="border-neutral-300 w-full md:w-auto hover:text-rose-300 transition-all duration-300 ease-in-out"
+                    >
+                      {menu_item.has_subitems ? (
+                        <DropdownItem subitems={menu_item.subitems}>
                           <Link
                             to={menu_item.url}
                             className="inline-block px-4 py-2 font-bold"
                           >
                             {menu_item.name}
                           </Link>
-                        )}
-                      </li>
-                    );
-                  })}
-                </ul>
-              }
+                        </DropdownItem>
+                      ) : (
+                        <Link
+                          to={menu_item.url}
+                          className="inline-block px-4 py-2 font-bold"
+                        >
+                          {menu_item.name}
+                        </Link>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
         </nav>
--- frontend/client/src/components/Home/Home.jsx
+++ frontend/client/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useECommerce from "../../hooks/useECommerce";
+import { Link } from "react-router-dom";
 
 import Carousel from "../Carousel";
 import Categories from "../Categories";
@@ -10,6 +11,19 @@ const Home = () => {
   return (
     <>
       <Carousel />
+      <div className="bg-rose-700 p-4 w-full text-white">
+        <div className="flex justify-between items-center mx-auto max-w-4xl">
+          <span className="text-xl">
+            Descubre la oferta de dia que tenemos para ti
+          </span>
+          <Link
+            to={"/"}
+            className="border-2 hover:bg-white px-8 p-2 rounded-full text-white hover:text-rose-700 transition-all duration-300 ease-in-out"
+          >
+            Ofertas del dia
+          </Link>
+        </div>
+      </div>
       <div className="mx-2 sm:mx-20 md:mx-48 mb-16 px-2 py-10">
         <Categories />
       </div>
